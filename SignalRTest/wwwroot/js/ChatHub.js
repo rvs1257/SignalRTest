2e2066e142b38cc8000037733adf777334f61106
rvs1257 <Rudi.Steinbach@gmail.com>
2019-01-30 22:37:17 +0200

    Update styling. Implement browser notifications.

--- SignalRTest/wwwroot/js/ChatHub.js
+++ SignalRTest/wwwroot/js/ChatHub.js
@@ -1,25 +1,60 @@
 "use strict";
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var sendButton = document.getElementById("sendButton");
+var userInput = document.getElementById("userInput");
+var messageInput = document.getElementById("messageInput");
 //Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
+sendButton.disabled = true;
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    var encodedMsg = user + ": " + msg;
+    var messageDiv = document.createElement("div");
+    messageDiv.className = "w-100";
+    messageDiv.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(messageDiv);
+    if (user != userInput.value) {
+        notifyMe(messageDiv.innerText);
+    }
 });
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
+connection
+    .start()
+    .then(function () {
+    sendButton.disabled = false;
+})
+    .catch(function (err) {
     return console.error(err.toString());
 });
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+document
+    .getElementById("sendButton")
+    .addEventListener("click", function (event) {
+    var user = userInput.value;
+    var message = messageInput.value;
+    connection
+        .invoke("SendMessage", user, message)
+        .catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
+// https://developer.mozilla.org/en-US/docs/Web/API/notification
+function notifyMe(message) {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification(message);
+    }
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification(message);
+            }
+        });
+    }
+}
 //# sourceMappingURL=ChatHub.js.map
--- SignalRTest/wwwroot/js/ChatHub.ts
+++ SignalRTest/wwwroot/js/ChatHub.ts
@@ -1,29 +1,66 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var sendButton = document.getElementById("sendButton") as HTMLButtonElement;
+var userInput = document.getElementById("userInput") as HTMLInputElement;
+var messageInput = document.getElementById("messageInput") as HTMLInputElement;
 
 //Disable send button until connection is established
-(document.getElementById("sendButton") as HTMLButtonElement).disabled = true;
+sendButton.disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("ReceiveMessage", function (user: string, message: string) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    var encodedMsg = `${user}: ${msg}`;
+    var messageDiv = document.createElement("div");
+    messageDiv.className = "w-100";
+    messageDiv.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(messageDiv);
+
+    if (user != userInput.value) {
+        notifyMe(messageDiv.innerText);
+    }
 });
 
-connection.start().then(function () {
-    (document.getElementById("sendButton") as HTMLButtonElement).disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = (document.getElementById("userInput") as HTMLInputElement).value;
-    var message = (document.getElementById("messageInput") as HTMLInputElement).value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+connection
+    .start()
+    .then(function () {
+        sendButton.disabled = false;
+    })
+    .catch(function (err) {
         return console.error(err.toString());
     });
-    event.preventDefault();
-});
+
+document
+    .getElementById("sendButton")
+    .addEventListener("click", function (event) {
+        var user = userInput.value;
+        var message = messageInput.value;
+        connection
+            .invoke("SendMessage", user, message)
+            .catch(function (err) {
+                return console.error(err.toString());
+            });
+        event.preventDefault();
+    });
+
+// https://developer.mozilla.org/en-US/docs/Web/API/notification
+function notifyMe(message: string) {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification(message);
+    }
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification(message);
+            }
+        });
+    }
+}
